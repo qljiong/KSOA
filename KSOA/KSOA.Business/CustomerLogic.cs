c4db9d4363b077539429b484806a085777fea4dc
qljiong <qianliqingqiu@163.com>
2014-03-10 11:52:19 +0800

    划分后台操作区域
    划分后台操作区域

---
+++ KSOA/KSOA.Business/CustomerLogic.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KSOA.Business
+{
+    public class CustomerLogic : DbAccess
+    {
+        public string test()
+        {
+            var result= _db.Customer.Where(s=>s.ID==1).FirstOrDefault();
+            return result.Name;
+        }
+    }
+}
---
+++ KSOA/KSOAWeb/Areas/Admin/Controllers/DefaultController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KSOAWeb.Areas.Admin.Controllers
+{
+    public class DefaultController : Controller
+    {
+        //
+        // GET: /Admin/Default/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
